195c76630fdab1aeb13251daac8f83a0620246d6
5404101316 <wetmano@gmail.com>
2017-11-09 23:50:31 +0700

    test echo msg line

--- notify.php
+++ notify.php
@@ -1,10 +1,36 @@
 <?php
 
-$token ="RT2I23exAmlJgAHywZ5xpySfqmD9LqoToyKbDZztfKG";
+
 
 $message = 'close';
 
-linebot($message,$token);
+define('LINE_API',"https://notify-api.line.me/api/notify");
+ 
+$token ="RT2I23exAmlJgAHywZ5xpySfqmD9LqoToyKbDZztfKG"; //ใส่Token ที่copy เอาไว้
+$str = "Hello"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+ 
+$res = notify_message($str,$token);
+print_r($res);
+function notify_message($message,$token){
+ $queryData = array('message' => $message);
+ $queryData = http_build_query($queryData,'','&');
+ $headerOptions = array( 
+         'http'=>array(
+            'method'=>’POST’,
+            'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                      ."Authorization: Bearer ".$token."\r\n"
+                      ."Content-Length: “.strlen($queryData)."\r\n",
+            'content' => $queryData
+         ),
+ );
+ $context = stream_context_create($headerOptions);
+ $result = file_get_contents(LINE_API,FALSE,$context);
+ $res = json_decode($result);
+ return $res;
+}
+
+
+//linebot($message,$token);
 
 function linebot($message,$token){
 	
